dba7f7877c3873c357276165d5ad626e145a24e6
Corin Craciun <ccraciun@f0r6s14.codam.nl>
2024-11-26 10:50:12 +0100

    added some basic structs and images

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,54 @@
 /*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 09:45:41 by ccraciun          #+#    #+#             */
-/*   Updated: 2024/11/26 09:45:42 by ccraciun         ###   ########.fr       */
+/*   Updated: 2024/11/26 10:49:48 by ccraciun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdlib.h>
+#include "../MLX42/include/MLX42/MLX42.h"
+#include <stdio.h>
+#define WIDTH 512
+#define HEIGHT 512
+
+//STRUCTS
+typedef struct s_textures
+{
+	mlx_texture_t	*blue_brk;
+	mlx_texture_t	*grey_brk;
+	mlx_texture_t	*purple_brk;
+	mlx_texture_t	*yellow_brk;
+}	t_textures;
+
+typedef struct s_imgs
+{
+	mlx_image_t	*blue_brk;
+	mlx_image_t	*grey_brk;
+	mlx_image_t	*purple_brk;
+	mlx_image_t	*yellow_brk;
+}	t_imgs;
+
+typedef struct s_map
+{
+	char	**cell_value;
+	size_t	width;
+	size_t	height;
+	bool	valid;
+}	t_map;
+
+typedef struct s_player_pos
+{
+	size_t	x;
+	size_t	y;
+}	t_player_pos;
+
+typedef struct s_game
+{
+	mlx_t					*mlx;
+	t_player_pos			*player_pos;
+	t_map					*map;
+	t_imgs					*imgs;
+}	t_game;
+
+//FUNCTIONS
--- src/main.c
+++ src/main.c
@@ -6,11 +6,48 @@
 /*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 09:54:18 by ccraciun          #+#    #+#             */
-/*   Updated: 2024/11/26 09:54:30 by ccraciun         ###   ########.fr       */
+/*   Updated: 2024/11/26 10:42:11 by ccraciun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
+#include "../include/cub3d.h"
+
+static void mlx_error(void)
 {
-	return (0);
+	puts(mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
+
+int32_t	main(void)
+{
+	// Start mlx
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "Test", true);
+	if (!mlx)
+        error();
+
+	// Try to load the file
+	mlx_texture_t* texture = mlx_load_png("assets/blue_brick.png");
+	if (!texture)
+	{
+		printf("failed here\n");
+		error();
+	}
+	
+	// Convert texture to a displayable image
+	mlx_image_t* img = mlx_texture_to_image(mlx, texture);
+	if (!img)
+        error();
+
+	// Display the image
+	if (mlx_image_to_window(mlx, img, 0, 0) < 0)
+        error();
+
+	mlx_loop(mlx);
+
+	mlx_delete_image(mlx, img);
+	mlx_delete_texture(texture);
+
+        // Optional, terminate will clean up any leftover images (not textures!)
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
 }
