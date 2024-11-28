1c029e60c0fb0822b59da7f8ef0504243dda001a
eugene-png <95448366+eugene-png@users.noreply.github.com>
2024-11-28 21:47:17 +0100

    draft

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,37 @@
 /*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 09:45:41 by ccraciun          #+#    #+#             */
-/*   Updated: 2024/11/26 09:45:42 by ccraciun         ###   ########.fr       */
+/*   Updated: 2024/11/28 21:29:07 by erybolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <math.h>
+# include <stdio.h>
+# include "../MLX42/include/MLX42/MLX42.h"
+
+#define SCREEN_W 800
+#define SCREEN_H 600
+#define MAP_W 9
+#define MAP_H 10
+
+typedef struct    s_player
+{
+	float x;
+	float y;
+	float angle;
+	float fov;
+}	t_player;
+
+typedef struct    s_cub3d
+{
+	int				map[MAP_H][MAP_W];
+	t_player		player;
+	mlx_t*			mlx;
+	mlx_image_t*	mlx_image;
+	bool			keyState[512];
+}	t_cub3d;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -6,11 +6,59 @@
 /*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 09:54:18 by ccraciun          #+#    #+#             */
-/*   Updated: 2024/11/26 09:54:30 by ccraciun         ###   ########.fr       */
+/*   Updated: 2024/11/28 21:46:32 by erybolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/cub3d.h"
+
 int main(void)
 {
+	t_cub3d	cub3d;
+
+	cub3d.mlx = mlx_init(SCREEN_W, SCREEN_H, "nov 28 raycasting example", true);
+	if (!cub3d.mlx) {
+		fprintf(stderr, "MLX42 initialisation failed!\n");
+		return 1;
+	}
+
+	cub3d.mlx_image = mlx_new_image(cub3d.mlx, SCREEN_W, SCREEN_H);
+	if (!cub3d.mlx_image)
+	{
+		fprintf(stderr, "MLX42 image creation failed!\n");
+		mlx_terminate(cub3d.mlx);
+		return 1;
+	}
+
+	mlx_image_to_window(cub3d.mlx, cub3d.mlx_image, 0, 0);
+
+	cub3d.player.x = 1;
+	cub3d.player.y = 1;
+	cub3d.player.angle = 1;
+	cub3d.player.fov = M_PI_4;
+
+	const int map_example[10][9] = {
+		{1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 1, 0, 1, 0, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 1, 1, 0, 1, 0, 0, 1},
+		{1, 0, 1, 0, 0, 1, 0, 0, 1},
+		{1, 0, 0, 1, 0, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 1, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1}
+	};
+
+	for (int i = 0; i < MAP_H; i++)
+		for (int j = 0; j < MAP_W; j++)
+			cub3d.map[i][j] = map_example[i][j];
+
+	//mlx loop hook
+	//mlx key hook
+
+	mlx_loop(cub3d.mlx);
+	mlx_terminate(cub3d.mlx);
+
 	return (0);
 }
