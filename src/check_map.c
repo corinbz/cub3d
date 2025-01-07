52aeef4ef82b1d0410919a61be7f3f7299c72cc0
Corin Craciun <ccraciun@f1r1s19.codam.nl>
2025-01-07 13:20:09 +0100

    map validation seems like is working

--- src/check_map.c
+++ src/check_map.c
@@ -6,7 +6,7 @@
 /*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 11:43:57 by corin             #+#    #+#             */
-/*   Updated: 2025/01/07 12:26:46 by ccraciun         ###   ########.fr       */
+/*   Updated: 2025/01/07 13:19:13 by ccraciun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,14 @@ bool check_f_or_l(char *row)
 	int	i;
 
 	i = 0;
-	while(row[i])
+	// printf("%s",row);
+	while(row[i] && row[i] != '\n')
 	{
-		if(row[i] != '1' || row[i] != ' ')
+		if(row[i] != '1' && row[i] != ' ')
+		{
+			// printf("%c at pos %d",row[i], i);
 			return (false);
+		}
 		i++;
 	}
 	return (true);
@@ -46,7 +50,7 @@ bool check_sides(char *row)
 	first = '\0';
 	last = '\0';
 	i = 0;
-	printf("%s",row);
+	// printf("%s",row);
 	while(row[i])
 	{
 		if (row[i] != ' ' && !first)
@@ -79,6 +83,36 @@ bool	check_space_neighbours(char *row)
 	}
 	return (true);
 }
+bool check_longer_lines(char **array, int i)
+{
+	int	curr_len;
+	int	prev_len;
+	int	nxt_len;
+
+	curr_len = ft_strlen(array[i]);
+	prev_len = ft_strlen(array[i - 1]);
+	nxt_len = ft_strlen(array[i + 1]);
+	
+	if (curr_len > prev_len)
+	{
+		while(array[i][prev_len - 1] && array[i][prev_len - 1] != '\n')
+		{
+			if(array[i][prev_len - 1] != '1')
+				return (dsp_err("long line char must be 1", false));
+			prev_len++;
+		}
+	}
+	if (curr_len > nxt_len)
+	{
+		while(array[i][nxt_len - 1] && array[i][prev_len - 1] != '\n')
+		{
+			if(array[i][nxt_len - 1] != '1')
+				return (dsp_err("long line char must be 1", false));
+			nxt_len++;
+		}
+	}
+	return (true);
+}
 
 bool valid_map(t_map *map)
 {
@@ -94,12 +128,17 @@ bool valid_map(t_map *map)
 	j = 0;
 	while(i < max_rows)
 	{
-		if ((i == 0 || i == max_rows - 1) && check_f_or_l(map_array[i]))
+		if (i == 0 || i == max_rows - 1)
 		{
+			if(!check_f_or_l(map_array[i]))
+				return (dsp_err("First or last row contain invalid chars", false));
 			i++;
-			break;
+			continue;
 		}
-		if(!check_sides(map_array[i]) || !check_space_neighbours(map_array[i]))
+		printf("%s",map_array[i]);
+		if(!check_sides(map_array[i]) ||
+			!check_space_neighbours(map_array[i]) ||
+			!check_longer_lines(map_array, i))
 			return (false);
 		i++;
 	}
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 09:54:18 by ccraciun          #+#    #+#             */
-/*   Updated: 2025/01/07 12:25:12 by ccraciun         ###   ########.fr       */
+/*   Updated: 2025/01/07 13:08:18 by ccraciun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int main(void)
 	t_map	*map;
 	map = ft_calloc(1, sizeof(t_map));
 	parse_map_file("assets/maps/simple_valid.cub", map);
-	// valid_map(map);
-	printf("%d\n",valid_map(map));
+	valid_map(map);
+	// printf("%d\n",valid_map(map));
 	free_map(map);
 	return (0);
 }
