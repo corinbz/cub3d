e3aa3010b6ed4bfcfee5b656dab9de64e5482c90
Corin Craciun <ccraciun@f0r6s8.codam.nl>
2025-01-07 10:44:58 +0100

    update

--- src/check_map.c
+++ src/check_map.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   check_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: corin <corin@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 11:43:57 by corin             #+#    #+#             */
-/*   Updated: 2025/01/06 11:19:06 by corin            ###   ########.fr       */
+/*   Updated: 2025/01/07 10:29:47 by ccraciun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 #include <stdbool.h>
 
-size_t count_rows(char **cells)
+int count_rows(char **cells)
 {
-	size_t res;
+	int res;
 
 	res = 0;
 	while(cells[res])
@@ -36,21 +36,36 @@ bool check_f_or_l(char **map, int i, int j)
 
 bool valid_map(t_map *map)
 {
-	bool check;
-	int i;
-	int j;
-	size_t max_rows;
-	char **map_array;
+	// bool check;
+	int		i;
+	int		j;
+	int		max_rows;
+	char	**map_array;
+	char	first;
+	char	last;
 
 	map_array = map->cell_value;
+	char first = '\0';
+	char last = '\0';
 	max_rows = count_rows(map_array);
 	i = 0;
 	j = 0;
-	while(map_array[i][j])
+	while(i < max_rows)
 	{
-		if ((i == 0 || i == max_rows) && check_f_or_l(map_array, i , j))
-			i++;
-		
+		while(map_array[i][j])
+		{
+			if ((i == 0 || i == max_rows - 1) && check_f_or_l(map_array, i, j))
+				break;
+			printf("%c",map_array[i][j]);
+			if (map_array[i][j] != ' ' && !first)
+				first = map_array[i][j];
+			if (map_array[i][j] != ' ' && first)
+				first = map_array[i][j];
+				
+			j++;
+		}
+		j = 0;
+		i++;
 	}
 	return (true);
 }
