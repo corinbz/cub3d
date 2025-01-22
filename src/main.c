b9421f5fe9dbaf92f6258f9e56b8a390ac62a81e
eugene-png <95448366+eugene-png@users.noreply.github.com>
2025-01-22 14:24:23 +0100

    draft

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 09:54:18 by ccraciun          #+#    #+#             */
-/*   Updated: 2025/01/22 14:10:27 by erybolov         ###   ########.fr       */
+/*   Updated: 2025/01/22 14:23:53 by erybolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +82,7 @@ void game_loop(void *param)
         game->data.draw_end = game->data.line_height / 2 + SCREEN_H / 2;
         if (game->data.draw_end >= SCREEN_H)
             game->data.draw_end = SCREEN_H - 1;
+        x++;
     }
 }
 
@@ -101,31 +102,31 @@ void prepare_and_load_textures(t_game *game)
     if (!game->img || (mlx_image_to_window(game->mlx, game->img, 0, 0) < 0))
         print_mlx_error_and_exit();
 
-    // game->textures.wall_n = mlx_load_png("textures/wall_n.png");
-    // if (!game->textures.wall_n)
-    //     print_mlx_error_and_exit();
-    // game->textures.wall_s = mlx_load_png("textures/wall_s.png");
-    // if (!game->textures.wall_s)
-    //     print_mlx_error_and_exit();
-    // game->textures.wall_w = mlx_load_png("textures/wall_w.png");
-    // if (!game->textures.wall_w)
-    //     print_mlx_error_and_exit();
-    // game->textures.wall_e = mlx_load_png("textures/wall_e.png");
-    // if (!game->textures.wall_e)
-    //     print_mlx_error_and_exit();
-    //
-    // game->textures.wall_n_img = mlx_texture_to_image(game->mlx, game->textures.wall_n);
-    // if (!game->textures.wall_n_img)
-    //     print_mlx_error_and_exit();
-    // game->textures.wall_s_img = mlx_texture_to_image(game->mlx, game->textures.wall_s);
-    // if (!game->textures.wall_s_img)
-    //     print_mlx_error_and_exit();
-    // game->textures.wall_w_img = mlx_texture_to_image(game->mlx, game->textures.wall_w);
-    // if (!game->textures.wall_w_img)
-    //     print_mlx_error_and_exit();
-    // game->textures.wall_e_img = mlx_texture_to_image(game->mlx, game->textures.wall_e);
-    // if (!game->textures.wall_e_img)
-    //     print_mlx_error_and_exit();
+    game->textures.wall_n = mlx_load_png("textures/wall_n.png");
+    if (!game->textures.wall_n)
+        print_mlx_error_and_exit();
+    game->textures.wall_s = mlx_load_png("textures/wall_s.png");
+    if (!game->textures.wall_s)
+        print_mlx_error_and_exit();
+    game->textures.wall_w = mlx_load_png("textures/wall_w.png");
+    if (!game->textures.wall_w)
+        print_mlx_error_and_exit();
+    game->textures.wall_e = mlx_load_png("textures/wall_e.png");
+    if (!game->textures.wall_e)
+        print_mlx_error_and_exit();
+
+    game->textures.wall_n_img = mlx_texture_to_image(game->mlx, game->textures.wall_n);
+    if (!game->textures.wall_n_img)
+        print_mlx_error_and_exit();
+    game->textures.wall_s_img = mlx_texture_to_image(game->mlx, game->textures.wall_s);
+    if (!game->textures.wall_s_img)
+        print_mlx_error_and_exit();
+    game->textures.wall_w_img = mlx_texture_to_image(game->mlx, game->textures.wall_w);
+    if (!game->textures.wall_w_img)
+        print_mlx_error_and_exit();
+    game->textures.wall_e_img = mlx_texture_to_image(game->mlx, game->textures.wall_e);
+    if (!game->textures.wall_e_img)
+        print_mlx_error_and_exit();
 
 
     int map[MAP_H][MAP_W] = {
@@ -155,7 +156,6 @@ int main() {
     t_game game = {0};
 
     prepare_and_load_textures(&game);
-
 
     // mlx_key_hook(game.mlx, key_callback, &game);
     mlx_loop_hook(game.mlx, game_loop, &game);
