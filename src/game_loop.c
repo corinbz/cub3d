17776ebfb6c921ea11f21f37020d376789054aa9
eugene-png <95448366+eugene-png@users.noreply.github.com>
2025-02-18 13:06:53 +0100

    norminette fix

--- src/game_loop.c
+++ src/game_loop.c
@@ -3,61 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   game_loop.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
+/*   By: erybolov <erybolov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 09:24:17 by erybolov          #+#    #+#             */
-/*   Updated: 2025/02/18 12:18:10 by erybolov         ###   ########.fr       */
+/*   Updated: 2025/02/18 13:06:32 by erybolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-void    draw_ceiling(const t_game *game)
+void	draw_ceiling(const t_game *game)
 {
-    int x;
-    int y;
+	int	x;
+	int	y;
 
-    y = 0;
-    while (y < SCREEN_H / 2)
-    {
-        x = 0;
-        while (x < SCREEN_W)
-        {
-            mlx_put_pixel(game->main_img, x, y,
-                get_rgba(game->map->ceiling_color[0], game->map->ceiling_color[1], game->map->ceiling_color[2], 255));
-            x++;
-        }
-        y++;
-    }
+	y = 0;
+	while (y < SCREEN_H / 2)
+	{
+		x = 0;
+		while (x < SCREEN_W)
+		{
+			mlx_put_pixel(game->main_img, x, y,
+				get_rgba(game->map->ceiling_color[0], \
+				game->map->ceiling_color[1], game->map->ceiling_color[2], 255));
+			x++;
+		}
+		y++;
+	}
 }
 
-void draw_floor(const t_game *game)
+void	draw_floor(const t_game *game)
 {
-    int x;
-    int y;
+	int	x;
+	int	y;
 
-    y = SCREEN_H / 2;
-    while (y < SCREEN_H)
-    {
-        x = 0;
-        while (x < SCREEN_W)
-        {
-            mlx_put_pixel(game->main_img, x, y, get_rgba(game->map->floor_color[0], game->map->floor_color[1], game->map->floor_color[2], 255));
-            x++;
-        }
-        y++;
-    }
+	y = SCREEN_H / 2;
+	while (y < SCREEN_H)
+	{
+		x = 0;
+		while (x < SCREEN_W)
+		{
+			mlx_put_pixel(game->main_img, x, y, \
+			get_rgba(game->map->floor_color[0], game->map->floor_color[1], \
+			game->map->floor_color[2], 255));
+			x++;
+		}
+		y++;
+	}
 }
 
-void game_loop(void *param)
+void	game_loop(void *param)
 {
-    t_game  *game;
+	t_game	*game;
 
-    game = (t_game *)param;
-    check_keys_w_s(game);
-    check_keys_a_d(game);
-    draw_ceiling(game);
-    draw_floor(game);
-
-    raycast(game);
+	game = (t_game *)param;
+	check_keys_w_s(game);
+	check_keys_a_d(game);
+	draw_ceiling(game);
+	draw_floor(game);
+	raycast(game);
 }
--- src/keys_handlers.c
+++ src/keys_handlers.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   keys_handlers.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccraciun <ccraciun@student.42.fr>          +#+  +:+       +#+        */
+/*   By: erybolov <erybolov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 09:10:28 by erybolov          #+#    #+#             */
-/*   Updated: 2025/02/18 12:13:40 by erybolov         ###   ########.fr       */
+/*   Updated: 2025/02/18 13:04:24 by erybolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,50 +30,71 @@ void	mouse_handler(double xpos, double ypos, void *param)
 	game->data.mouse_y = ypos;
 }
 
-void check_keys_w_s(t_game* g)
+void	check_keys_w_s(t_game *g)
 {
-    double  move_speed;
+	double	move_speed;
 
-    move_speed = g->mlx->delta_time * 5.0;
-    if (mlx_is_key_down(g->mlx, MLX_KEY_W))
-    {
-        if (g->map->cell_value[(int)g->data.pos_y][(int)(g->data.pos_x + g->data.dir_x * move_speed)] == '0')
-            g->data.pos_x += g->data.dir_x * move_speed;
-        if (g->map->cell_value[(int)(g->data.pos_y + g->data.dir_y * move_speed)][(int)g->data.pos_x] == '0')
-            g->data.pos_y += g->data.dir_y * move_speed;
-    }
-    if (mlx_is_key_down(g->mlx, MLX_KEY_S))
-    {
-        if (g->map->cell_value[(int)g->data.pos_y][(int)(g->data.pos_x - g->data.dir_x * move_speed)] == '0')
-            g->data.pos_x -= g->data.dir_x * move_speed;
-        if (g->map->cell_value[(int)(g->data.pos_y - g->data.dir_y * move_speed)][(int)g->data.pos_x] == '0')
-            g->data.pos_y -= g->data.dir_y * move_speed;
-    }
+	move_speed = g->mlx->delta_time * 5.0;
+	if (mlx_is_key_down(g->mlx, MLX_KEY_W))
+	{
+		if (g->map->cell_value[(int)g->data.pos_y] \
+		[(int)(g->data.pos_x + g->data.dir_x * move_speed)] == '0')
+			g->data.pos_x += g->data.dir_x * move_speed;
+		if (g->map->cell_value[(int)(g->data.pos_y + g->data.dir_y * \
+		move_speed)][(int)g->data.pos_x] == '0')
+			g->data.pos_y += g->data.dir_y * move_speed;
+	}
+	if (mlx_is_key_down(g->mlx, MLX_KEY_S))
+	{
+		if (g->map->cell_value[(int)g->data.pos_y] \
+		[(int)(g->data.pos_x - g->data.dir_x * move_speed)] == '0')
+			g->data.pos_x -= g->data.dir_x * move_speed;
+		if (g->map->cell_value[(int)(g->data.pos_y - g->data.dir_y * \
+		move_speed)][(int)g->data.pos_x] == '0')
+			g->data.pos_y -= g->data.dir_y * move_speed;
+	}
 }
 
-void check_keys_a_d(t_game* g)
+static void	check_key_d(t_game *g)
 {
-    double  rot_speed;
-    double old_dir_x;
-    double old_plane_x;
+	double	old_dir_x;
+	double	old_plane_x;
 
-    rot_speed = g->mlx->delta_time * 2.0;
-    if (mlx_is_key_down(g->mlx, MLX_KEY_D) || g->data.mouse_x > (3 * SCREEN_W / 4))
-    {
-        old_dir_x = g->data.dir_x;
-        g->data.dir_x = g->data.dir_x * cos(-rot_speed) - g->data.dir_y * sin(-rot_speed);
-        g->data.dir_y = old_dir_x * sin(-rot_speed) + g->data.dir_y * cos(-rot_speed);
-        old_plane_x = g->data.plane_x;
-        g->data.plane_x = g->data.plane_x * cos(-rot_speed) - g->data.plane_y * sin(-rot_speed);
-        g->data.plane_y = old_plane_x * sin(-rot_speed) + g->data.plane_y * cos(-rot_speed);
-    }
-    if (mlx_is_key_down(g->mlx, MLX_KEY_A) || g->data.mouse_x < (SCREEN_W / 4))
-    {
-        old_dir_x = g->data.dir_x;
-        g->data.dir_x = g->data.dir_x * cos(rot_speed) - g->data.dir_y * sin(rot_speed);
-        g->data.dir_y = old_dir_x * sin(rot_speed) + g->data.dir_y * cos(rot_speed);
-        old_plane_x = g->data.plane_x;
-        g->data.plane_x = g->data.plane_x * cos(rot_speed) - g->data.plane_y * sin(rot_speed);
-        g->data.plane_y = old_plane_x * sin(rot_speed) + g->data.plane_y * cos(rot_speed);
-    }
+	if (mlx_is_key_down(g->mlx, MLX_KEY_A) || g->data.mouse_x < (SCREEN_W / 4))
+	{
+		old_dir_x = g->data.dir_x;
+		g->data.dir_x = g->data.dir_x * cos(rot_speed) - \
+		g->data.dir_y * sin(rot_speed);
+		g->data.dir_y = old_dir_x * sin(rot_speed) + \
+		g->data.dir_y * cos(rot_speed);
+		old_plane_x = g->data.plane_x;
+		g->data.plane_x = g->data.plane_x * cos(rot_speed) - \
+		g->data.plane_y * sin(rot_speed);
+		g->data.plane_y = old_plane_x * sin(rot_speed) + \
+		g->data.plane_y * cos(rot_speed);
+	}
+}
+
+void	check_keys_a_d(t_game *g)
+{
+	double	rot_speed;
+	double	old_dir_x;
+	double	old_plane_x;
+
+	rot_speed = g->mlx->delta_time * 2.0;
+	if (mlx_is_key_down(g->mlx, MLX_KEY_D) || \
+	g->data.mouse_x > (3 * SCREEN_W / 4))
+	{
+		old_dir_x = g->data.dir_x;
+		g->data.dir_x = g->data.dir_x * cos(-rot_speed) - \
+		g->data.dir_y * sin(-rot_speed);
+		g->data.dir_y = old_dir_x * sin(-rot_speed) + \
+		g->data.dir_y * cos(-rot_speed);
+		old_plane_x = g->data.plane_x;
+		g->data.plane_x = g->data.plane_x * cos(-rot_speed) - \
+		g->data.plane_y * sin(-rot_speed);
+		g->data.plane_y = old_plane_x * sin(-rot_speed) + \
+		g->data.plane_y * cos(-rot_speed);
+	}
+	check_key_d(g);
 }
